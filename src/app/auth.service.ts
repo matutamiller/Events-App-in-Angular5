b20002af674420320254274120c6705d47ab2442
matutamiller <34442764+matutamiller@users.noreply.github.com>
2018-04-16 20:32:59 +0200

    implemented logout

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
+import { Router } from '@angular/router';
+
 @Injectable()
 export class AuthService {
 
   private _registerUrl = "http://localhost:3000/api/register";
   private _loginUrl = "http://localhost:3000/api/login";
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private _router: Router) { }
 
   registerUser(user){
     return this.http.post<any>(this._registerUrl, user)
@@ -18,7 +20,12 @@ export class AuthService {
   loggedIn(){
     return !!localStorage.getItem('token');
   }
-   
+  
+  logoutUser(){
+    localStorage.removeItem('token');
+    this._router.navigate(['/events']);
+  }  
+
   getToken(){
     return localStorage.getItem('token');
   }
